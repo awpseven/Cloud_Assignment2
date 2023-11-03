6ff6525adaf77445939d4b8f328673344a2e1f8d
awpseven <sunqy016@mymail.unisa.edu.au>
2023-11-04 01:04:47 +0800

    adding multi-thread in nuberDispatch

--- src/nuber/students/NuberDispatch.java
+++ src/nuber/students/NuberDispatch.java
@@ -1,35 +1,36 @@
 package nuber.students;
 
 import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
-
+import java.util.concurrent.ArrayBlockingQueue;
 /**
  * The core Dispatch class that instantiates and manages everything for Nuber
- * 
+ *
  * @author james
  *
  */
 
-// Dispatch handles everything to do with running an instance of Nuber: 
+// Dispatch handles everything to do with running an instance of Nuber:
 // adding/providing drivers, booking passengers, and when required, shutting everything down.
 
-// It contains a map of the regions it’s responsible for, and a central collection of Drivers awaiting a booking. 
-// It also has a variable, set by its constructor, for whether the reportEvent() function prints information out to the console. 
+// It contains a map of the regions it’s responsible for, and a central collection of Drivers awaiting a booking.
+// It also has a variable, set by its constructor, for whether the reportEvent() function prints information out to the console.
 // You can use this function to print out debugging information if required.
 
 
-// When a passenger is booked into dispatch using bookPassenger(), 
-// dispatch is also told which region the job is in. Dispatch then needs to get that region, 
+// When a passenger is booked into dispatch using bookPassenger(),
+// dispatch is also told which region the job is in. Dispatch then needs to get that region,
 // and book the passenger into that specific “Nuber Region” using the region’s bookPassenger() function.
 
-// Drivers are allocated on a first-in, first-out basis. Because passengers are allocated to a specific region, 
+// Drivers are allocated on a first-in, first-out basis. Because passengers are allocated to a specific region,
 // different regions will be accessing a central collection of available drivers from Dispatch.
 
 public class NuberDispatch {
 
 	/**
-	 * The maximum number of idle drivers that can be awaiting a booking 
+	 * The maximum number of idle drivers that can be awaiting a booking
 	 */
 	private final int MAX_DRIVERS = 999;
 	public final AtomicInteger ID = new AtomicInteger();
@@ -37,10 +38,16 @@ public class NuberDispatch {
 	private boolean logEvents = false;
 
 	private HashMap<String, Integer> regionInfo;
+
+	private BlockingQueue<Driver> driverList;
+	private HashMap<String, NuberRegion> regionMap;
+
+	private int awaitingDriver;
+
 	/**
 	 * Creates a new dispatch objects and instantiates the required regions and any other objects required.
 	 * It should be able to handle a variable number of regions based on the HashMap provided.
-	 * 
+	 *
 	 * @param regionInfo Map of region names and the max simultaneous bookings they can handle
 	 * @param logEvents Whether logEvent should print out events passed to it
 	 */
@@ -48,76 +55,112 @@ public class NuberDispatch {
 	{
 		this.regionInfo = regionInfo;
 		this.logEvents = logEvents;
+		this.awaitingDriver = 0;
+		this.driverList = new ArrayBlockingQueue<Driver>(MAX_DRIVERS);
+		this.regionMap = new HashMap<String, NuberRegion>();
+		System.out.println("[NuberDispatch] is creating " + this.regionInfo.size() + "Nuber Dispatch");
+		for(var region: this.regionInfo.entrySet()) {
+
+			String Name = region.getKey();
+			Integer maxDelay = region.getValue();
+			System.out.println("Creating [NumberRegions]" + Name + " ");
+
+			NuberRegion _region = new NuberRegion(this, Name, maxDelay);
+			regionMap.put(Name, _region);
+			System.out.println("[NumberRegions]" + Name + "  created successfully.");
+		}
 	}
-	
+
 	/**
 	 * Adds drivers to a queue of idle driver.
-	 *  
+	 *
 	 * Must be able to have drivers added from multiple threads.
-	 * 
+	 *
 	 * @param The driver to add to the queue.
 	 * @return Returns true if driver was added to the queue
 	 */
 	public boolean addDriver(Driver newDriver)
 	{
+		try {
+			driverList.put(newDriver);
+			awaitingDriver++;
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-	
+
 	/**
 	 * Gets a driver from the front of the queue
-	 *  
+	 *
 	 * Must be able to have drivers added from multiple threads.
-	 * 
+	 *
 	 * @return A driver that has been removed from the queue
 	 */
 	public Driver getDriver()
 	{
+		try {
+			Driver valDriver = driverList.take();
+			awaitingDriver--;
+			return valDriver;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	/**
 	 * Prints out the string
 	 * 	    booking + ": " + message
 	 * to the standard output only if the logEvents variable passed into the constructor was true
-	 * 
+	 *
 	 * @param booking The booking that's responsible for the event occurring
 	 * @param message The message to show
 	 */
 	public void logEvent(Booking booking, String message) {
-		
 		if (!logEvents) return;
-		
-		System.out.println(booking + ": " + message);
-		
+		System.out.println("[Booking]" +booking + ": " + message);
+
 	}
 
 	/**
 	 * Books a given passenger into a given Nuber region.
-	 * 
+	 *
 	 * Once a passenger is booked, the getBookingsAwaitingDriver() should be returning one higher.
-	 * 
+	 *
 	 * If the region has been asked to shutdown, the booking should be rejected, and null returned.
-	 * 
+	 *
 	 * @param passenger The passenger to book
 	 * @param region The region to book them into
 	 * @return returns a Future<BookingResult> object
 	 */
 	public Future<BookingResult> bookPassenger(Passenger passenger, String region) {
+		NuberRegion allocatedRegion = regionMap.get(region);
+		Future<BookingResult> futureResult = allocatedRegion.bookPassenger(passenger);
+		return futureResult;
 	}
 
 	/**
 	 * Gets the number of non-completed bookings that are awaiting a driver from dispatch
-	 * 
+	 *
 	 * Once a driver is given to a booking, the value in this counter should be reduced by one
-	 * 
+	 *
 	 * @return Number of bookings awaiting driver, across ALL regions
 	 */
 	public int getBookingsAwaitingDriver()
 	{
+		return awaitingDriver;
 	}
-	
+
 	/**
 	 * Tells all regions to finish existing bookings already allocated, and stop accepting new bookings
 	 */
 	public void shutdown() {
+		for(NuberRegion region: regionMap.values()) {
+			region.shutdown();
+		}
 	}
 
 }
